2bb56166b7c90f53ffd46236a42ed0674bf99b14
baasirishfaq <baasirishfaq@gmail.com>
2023-11-29 17:31:54 +0530

    prac

--- dojo.cpp
+++ dojo.cpp
@@ -7,19 +7,29 @@ using namespace std;
 
 int main()
 {
-    int n, arr[n];
+    int n, max = 0;
     cout << "enter limit for array :";
     cin >> n;
-
+    int arr[n];
+    cout << "enter elements in array :";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    cout << "elements of array are";
+    cout << "elements of array are: " << endl;
     {
         for (int i = 0; i < n; i++)
         {
-            cout << arr[i];
+            cout << arr[i] << endl;
         }
     }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << "largest number " << max;
 }
