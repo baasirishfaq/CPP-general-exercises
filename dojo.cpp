62a4eb94795a4b4d33d0cb0bba2d346935a272a6
baasirishfaq <baasirishfaq@gmail.com>
2023-12-02 23:05:14 +0530

    prac sesh

--- dojo.cpp
+++ dojo.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main()
 {
-    int stockprice[6] = {7, 1, 5, 3, 6, 4};
+    int cost;
+    int stockprice[6]{7, 1, 5, 3, 6, 4};
     int mini = stockprice[0], profit = 0;
 
     for (int i = 1; i < 6; i++)
     {
-        int cost = stockprice[i] - mini;
+        cost = stockprice[i] - mini;
         profit = max(profit, cost);
         mini = min(mini, stockprice[i]);
     }
