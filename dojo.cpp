bc3b42cf5a9b87e6899772a1301ef40b44c54c0b
baasirishfaq <baasirishfaq@gmail.com>
2023-12-05 23:55:28 +0530

    prac

--- dojo.cpp
+++ dojo.cpp
@@ -5,23 +5,9 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cout << "enter array lim :";
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    cout << "array elements are :" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i];
-    }
-    cout << "reverse array is : " << endl;
-    for (int j = n - 1; j >= 0; j--)
-    {
-        cout << arr[j];
-    }
+    int a, b, sum = 0;
+    cout << "enter 2 numbers :";
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum;
 }
