aa348f21d743f154fd98d46a0f6b4c1bacb18ef3
baasirishfaq <baasirishfaq@gmail.com>
2024-01-05 21:17:47 +0530

    prac

--- dojo.cpp
+++ dojo.cpp
@@ -5,35 +5,11 @@ using namespace std;
 
 int main()
 {
-    int n, flag = 0;
-    cout << "enter array limit ";
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    for (int j = 0; j < n;)
-    {
-        for (int k = j + 1; k < n; k++)
-        {
-            if (arr[j] == arr[k])
-            {
-                flag = 1;
-                cout << "element " << arr[k] << " has a duplicate" << endl;
-            }
-        }
-        j++;
-    }
-
-    if (flag == 1)
-    {
-        // only to check if there is a duplicate without specifing which element has a duplicate....
-    }
-    else
-    {
-        cout << "no duplicate in array ";
-    }
-};
+    int a, b, div;
+    cout << "enter number ";
+    cin >> a;
+    cout << "enter number ";
+    cin >> b;
+    div = a / b;
+    cout << div;
+}
