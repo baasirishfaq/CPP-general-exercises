b57fe142f7714ea3ab0ad7edba22489a01684a5a
baasirishfaq <baasirishfaq@gmail.com>
2024-01-06 23:03:01 +0530

    .

--- dojo.cpp
+++ dojo.cpp
@@ -5,23 +5,27 @@ using namespace std;
 
 int main()
 {
+
     int n;
     cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 4 == 0)
+        {
+            continue;
+        }
 
-    if (n % 3 == 0 && n % 5 == 0)
-    {
-        cout << "fizbuxx" << endl;
-    }
-    else if (n % 3 == 0)
-    {
-        cout << "fizz";
-    }
-    else if (n % 5 == 0)
-    {
-        cout << "buxx";
-    }
-    else
-    {
-        cout << "nothing";
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            cout << "fizzbuzz";
+        }
+        else if (i % 3 == 0)
+        {
+            cout << "fizz";
+        }
+        else if (i % 5 == 0)
+        {
+            cout << "buzz";
+        }
     }
 }
