c42cf333ab3ef66a6a150513db90a62b707931ee
baasirishfaq <baasirishfaq@gmail.com>
2024-01-24 01:44:54 +0530

    leetcode pallindrome string

--- algoclass.cpp
+++ algoclass.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-#include<stdio.h>
- 
+#include <bits/stdc++.h>
+
 using namespace std;
- 
+
 int main()
 {
-  
+    int x;
+    cout << "enter numeber ";
+    cin >> x;
+    string s = to_string(x);
+    reverse(s.begin(), s.end());
+    int m = stoi(s);
+    cout << m;
 }
