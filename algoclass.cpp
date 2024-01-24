1ad4b8060a946f82d112e7905f854decf95c894e
baasirishfaq <baasirishfaq@gmail.com>
2024-01-24 21:47:57 +0530

    how to take input along with spaces.

--- algoclass.cpp
+++ algoclass.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int main()
 {
-    int x;
-    cout << "enter numeber ";
-    cin >> x;
-    string s = to_string(x);
-    reverse(s.begin(), s.end());
-    int m = stoi(s);
-    cout << m;
+    string para;
+    getline(cin, para, '\0');
+    cout << para;
+    return 0;
 }
