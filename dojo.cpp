0ea4dd0b0d77966ec0553ca27891011b8559f36c
baasirishfaq <baasirishfaq@gmail.com>
2024-09-23 14:48:29 -0400

    school work

--- dojo.cpp
+++ dojo.cpp
@@ -1,28 +1,35 @@
-#include <iostream>
 #include <stdio.h>
-
-using namespace std;
 
 int main()
 {
+    int amount, five, ten, twenty, fifty, hundred, remainder;
 
-    int n;
-    cin >> n;
-    int digitct = 0;
+    printf("Enter a amount in $: ");
+    scanf("%d", &amount);
 
-    if (n < 1000000000)
-    {
-        for (int num = n; num > 0; num = num / 10)
-        {
-            digitct++;
-        }
-    }
-    if (n > 1000000000)
-    {
-        for (int num = n; num > 0; num = num / 100)
-        {
-            digitct++;
-        }
-    }
-    cout << "it has " << digitct << " digits";
+    hundred = amount / 100;
+    remainder = amount % 100;
+
+    fifty = remainder / 50;
+    remainder %= 50;
+
+    twenty = remainder / 20;
+    remainder %= 20;
+
+    ten = remainder / 10;
+    remainder %= 10;
+
+    five = remainder / 5;
+    remainder %= 5;
+
+    printf("You can pay the amount in:\n");
+    printf("%d x $100 bills\n", hundred);
+    printf("%d x $50 bills\n", fifty);
+    printf("%d x $20 bills\n", twenty);
+    printf("%d x $10 bills\n", ten);
+    printf("%d x $5 bills\n", five);
+
+    printf("Remaining amount is: $%d\n", remainder);
+
+    return 0;
 }
